161b433a63d3992557d888f49d42fa87b29f326c
Tim Smith <tdsmith@mozilla.com>
2018-11-08 15:51:47 -0800

    Don't fail if database doesn't exist

--- render.sh
+++ render.sh
@@ -2,7 +2,7 @@
 set -e
 set -o pipefail
 
-rm webrender.sqlite3
+rm -f webrender.sqlite3
 dbfs cp dbfs:/tdsmith/webrender.sqlite3 .
 python burndown.py
 Rscript -e 'rmarkdown::render("dashboard.Rmd")' > rmarkdown.log 2>&1
