cc266bd3aa418130476ec07b6e6f458995dd2e57
Tim Smith <tdsmith@mozilla.com>
2018-11-08 13:50:50 -0800

    Customize burndown script

--- burndown.py
+++ burndown.py
@@ -1,46 +1,46 @@
 # https://extremely-alpha.iodide.io/notebooks/31/
-
-import requests
 from datetime import datetime, timedelta
-import pandas as pd; from pandas import DataFrame, Series
+
+import pandas as pd
+import requests
 
 meta_bug_id = 1386669
-url = """https://bugzilla.mozilla.org/rest/bug?blocks={bug_id}&include_fields=id,priority,last_change_time,status,creation_time""".format(bug_id=meta_bug_id)
+url = (
+    "https://bugzilla.mozilla.org/rest/bug?"
+    "blocks={bug_id}&include_fields=id,priority,last_change_time,status,creation_time"
+    .format(bug_id=meta_bug_id)
+)
 
 response = requests.get(url)
-bugs = response.json()['bugs']
-
-def extract_counts(priority, start_date='2018-01-01'):
-  start = datetime.strptime(start_date, "%Y-%m-%d")
-  end = datetime.today()
-  day = timedelta(days=1)
-
-  open_counts = []
-
-  bugs_by_priority = [b for b in bugs if b['priority']==priority]
-
-  curr_date = start
-
-  while curr_date < end:
-    open_bugs = [b for b in bugs_by_priority if datetime.strptime(b["creation_time"][0:10], "%Y-%m-%d")<=curr_date and not (b["status"] in ('RESOLVED', 'VERIFIED') and datetime.strptime(b['last_change_time'][0:10], "%Y-%m-%d")<=curr_date )]
-    open_counts.append((curr_date, len(open_bugs)))
-    curr_date += day
-
-  return open_counts
+bugs = response.json()["bugs"]
 
 
-def plot_burndown_matplotlib(priority, start_date='2018-01-01'):
-  # usage: display(plot_burndown_matplotlib('P1'))
-  open_counts = extract_counts(priority, start_date)
+def extract_counts(priority, start_date="2018-01-01"):
+    start = datetime.strptime(start_date, "%Y-%m-%d")
+    end = datetime.today()
+    day = timedelta(days=1)
 
-  open_counts_df = pd.DataFrame(open_counts,columns=['Day',priority])
-  fig = open_counts_df.plot(
-    kind='line',
-    x='Day',
-    title='{priority} open bugs'.format(priority=priority),
-    legend=False
-  )
-  ylim = fig.get_ylim()
-  fig.set_ylim([0, ylim[1]])
+    open_counts = []
 
-  return fig.figure.tight_layout()
+    bugs_by_priority = [b for b in bugs if b["priority"] == priority]
+
+    curr_date = start
+
+    while curr_date < end:
+        open_bugs = [
+            b
+            for b in bugs_by_priority
+            if datetime.strptime(b["creation_time"][0:10], "%Y-%m-%d") <= curr_date
+            and not (
+                b["status"] in ("RESOLVED", "VERIFIED")
+                and datetime.strptime(b["last_change_time"][0:10], "%Y-%m-%d")
+                <= curr_date
+            )
+        ]
+        open_counts.append((curr_date, len(open_bugs)))
+        curr_date += day
+
+    return pd.DataFrame(open_counts, columns=["Day", "Count"]).assign(Priority=priority)
+
+
+pd.concat(extract_counts(i) for i in ("P1", "P2", "P3")).to_csv("bugzilla.csv", index=False)
