4dc7dd0ffd4334957e810b1dfda101a139c1dd71
Tim Smith <tdsmith@mozilla.com>
2019-01-10 13:04:17 -0800

    Rely on the new defaults

--- render.sh
+++ render.sh
@@ -3,8 +3,8 @@ set -e
 set -o pipefail
 
 rm -f webrender.sqlite3 webrender_nvidia.sqlite3
-dbfs cp dbfs:/tdsmith/webrender.sqlite3 .
+# dbfs cp dbfs:/tdsmith/webrender.sqlite3 .
 dbfs cp dbfs:/tdsmith/webrender_nvidia.sqlite3 .
 python burndown.py
 Rscript -e 'rmarkdown::render("dashboard.Rmd")' > rmarkdown.log 2>&1
-Rscript -e 'rmarkdown::render("dashboard.Rmd", params=list(dbname="webrender_nvidia.sqlite3"), output_file="dashboard_nvidia.html")' > rmarkdown.log 2>&1
+# Rscript -e 'rmarkdown::render("dashboard.Rmd", params=list(dbname="webrender_nvidia.sqlite3"), output_file="dashboard_nvidia.html")' > rmarkdown.log 2>&1
