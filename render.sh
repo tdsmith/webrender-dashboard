d735f05bea882e2da1bd97548abb494fb0e50a86
Tim Smith <tdsmith@mozilla.com>
2019-02-01 12:43:20 -0800

    Render AMD dashboard

--- render.sh
+++ render.sh
@@ -2,9 +2,9 @@
 set -e
 set -o pipefail
 
-rm -f webrender.sqlite3 webrender_nvidia.sqlite3
-# dbfs cp dbfs:/tdsmith/webrender.sqlite3 .
+rm -f webrender_*.sqlite3
 dbfs cp dbfs:/tdsmith/webrender_nvidia.sqlite3 .
+dbfs cp dbfs:/tdsmith/webrender_amd.sqlite3 .
 python burndown.py
-Rscript -e 'rmarkdown::render("dashboard.Rmd")' > rmarkdown.log 2>&1
-# Rscript -e 'rmarkdown::render("dashboard.Rmd", params=list(dbname="webrender_nvidia.sqlite3"), output_file="dashboard_nvidia.html")' > rmarkdown.log 2>&1
+Rscript -e 'rmarkdown::render("dashboard.Rmd", params=list(dbname="webrender_nvidia.sqlite3"), output_file="dashboard_nvidia.html")' > rmarkdown.log 2>&1
+Rscript -e 'rmarkdown::render("dashboard.Rmd", params=list(dbname="webrender_amd.sqlite3"), output_file="dashboard_amd.html")' >> rmarkdown.log 2>&1
