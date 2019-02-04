ea25ece042a20789b3c2d5f286713a6a12bb35aa
Tim Smith <tdsmith@mozilla.com>
2019-02-04 14:28:03 -0800

    Render Intel dashboard

--- render.sh
+++ render.sh
@@ -5,6 +5,8 @@ set -o pipefail
 rm -f webrender_*.sqlite3
 dbfs cp dbfs:/tdsmith/webrender_nvidia.sqlite3 .
 dbfs cp dbfs:/tdsmith/webrender_amd.sqlite3 .
+dbfs cp dbfs:/tdsmith/webrender_intel.sqlite3 .
 python burndown.py
 Rscript -e 'rmarkdown::render("dashboard.Rmd", params=list(dbname="webrender_nvidia.sqlite3"), output_file="dashboard_nvidia.html")' > rmarkdown.log 2>&1
 Rscript -e 'rmarkdown::render("dashboard.Rmd", params=list(dbname="webrender_amd.sqlite3"), output_file="dashboard_amd.html")' >> rmarkdown.log 2>&1
+Rscript -e 'rmarkdown::render("dashboard.Rmd", params=list(dbname="webrender_intel.sqlite3"), output_file="dashboard_intel.html")' >> rmarkdown.log 2>&1
